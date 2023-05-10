287459fca0682aa1b1fecde55152c44281e67c56
suchethaleonidou <Suchethao>
2023-05-10 16:43:05 -0400

    clerk is working and basic framework for frontend is built. Moving to shell backend

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,34 @@
-import { ClerkProvider, RedirectToSignIn, SignedIn, SignedOut } from '@clerk/nextjs';
-import { UserButton } from '@clerk/nextjs';
-import { useRouter } from 'next/router';
-import styles from '../styles/Dashboard.module.css';
+import { ClerkProvider, RedirectToSignIn, SignedIn, SignedOut } from '@clerk/nextjs'
+import { UserButton } from '@clerk/nextjs'
+import { useRouter } from 'next/router'
 
 export default function Home() {
-  const router = useRouter();
+  const router = useRouter()
 
   return (
     <ClerkProvider
-      frontendApi="ppk_test_dHJ1c3RlZC10dXJrZXktMzguY2xlcmsuYWNjb3VudHMuZGV2JA"
+      frontendApi={process.env.NEXT_PUBLIC_CLERK_FRONTEND_API}
       navigate={router.push}
     >
-      <nav>
+      <nav style={{ backgroundColor: 'black', color: 'white', padding: '10px' }}>
         <UserButton />
       </nav>
-      <main className={styles.container}>
+      <main>
         <SignedIn>
-          <h1 className={styles.title}>Welcome to your dashboard!</h1>
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <h1 style={{ fontSize: '24px', fontWeight: 'bold', color: 'blue' }}>Welcome to your dashboard!</h1>
+            <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '10px', padding: '10px' }}>
+              <div style={{ backgroundColor: 'white', height: '100px', border: '1px solid black', boxShadow: '2px 2px 5px #888888' }}></div>
+              <div style={{ backgroundColor: 'white', height: '100px', border: '1px solid black', boxShadow: '2px 2px 5px #888888' }}></div>
+              <div style={{ backgroundColor: 'white', height: '100px', border: '1px solid black', boxShadow: '2px 2px 5px #888888' }}></div>
+              <div style={{ backgroundColor: 'white', height: '100px', border: '1px solid black', boxShadow: '2px 2px 5px #888888' }}></div>
+              <div style={{ backgroundColor: 'white', height: '100px', border: '1px solid black', boxShadow: '2px 2px 5px #888888' }}></div>
+              <div style={{ backgroundColor: 'white', height: '100px', border: '1px solid black', boxShadow: '2px 2px 5px #888888' }}></div>
+            </div>
+            <footer style={{ backgroundColor: 'black', color: 'white', padding: '10px', marginTop: 'auto' }}>
+              <p style={{ textAlign: 'center' }}>About Us</p>
+            </footer>
+          </div>
         </SignedIn>
         <SignedOut>
           <RedirectToSignIn />
